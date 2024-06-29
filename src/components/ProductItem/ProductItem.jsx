89432980c0fd84de7df6906363c8652ddf8232a2
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-06-29 18:06:02 +0200

    Add PorudctItem component jsx and css file

---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import productItem from "./ProductItem.module.css";
+
+export default function ProductItem({
+  productData,
+  product,
+  productDescription,
+  productImage,
+  productTitle,
+}) {
+  return (
+    <>
+      <Link
+        to={"/shop/" + productData.key}
+        state={product}
+        className={productItem.productLink}
+      >
+        <div className={productItem.productContainer}>
+          <img
+            key={productDescription}
+            src={productImage}
+            alt=""
+            className={productItem.productImage}
+          />
+          <div className={productItem.productDescriptionContainer}>
+            <span className={productItem.productText}>{productTitle}</span>
+            <span className={productItem.productPrice}>
+              ${productData.price}
+            </span>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+}
