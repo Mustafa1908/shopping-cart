794d87046c43ed94a6517d54e2e510d34d911bbd
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-07-02 16:53:21 +0200

    Add elements for productAdded page

--- src/pages/ProductAdded/ProductAdded.jsx
+++ src/pages/ProductAdded/ProductAdded.jsx
@@ -1,23 +1,35 @@
-import { useLocation } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
+import productAdded from "./ProductAdded.module.css";
+import { ProductListContext } from "../../App";
+import { useContext } from "react";
 
 export default function ProductAdded() {
   let productArray = useLocation();
+  console.log(productArray);
   return (
-    <>
-      <p>Vous venez</p>
-      <p>d'ajouter un produit</p>
-      <img src={productArray.state.state.image} alt="" />
-      <br />
-      <Link to={"/shoppingcard"}>Aller au panier</Link>
-      <br />
-      <Link to={"/jewelery"}>jewelery</Link>
-      <br />
-      <Link to={"/electronics"}>electronics</Link>
-      <br />
-      <Link to={"/men"}>men clothes</Link>
-      <br />
-      <Link to={"/women"}>women clothes</Link>
-    </>
+    <main className={productAdded.main}>
+      <div className={productAdded.productAddedContainer}>
+        <img
+          src={productArray.state.state.image}
+          alt=""
+          className={productAdded.productImage}
+        />
+        <div className={productAdded.addedContainer}>
+          <span className={productAdded.checkMark}>&#10003;</span>
+          <span>Added to the cart</span>
+        </div>
+      </div>
+      <div className={productAdded.leaveContainer}>
+        <Link
+          to={productArray.pathname + "/shoppingcard"}
+          className={productAdded.checkoutButton}
+        >
+          Checkout
+        </Link>
+        <Link to={"/jewelery"} className={productAdded.continueShoppingButton}>
+          Continue Shopping
+        </Link>
+      </div>
+    </main>
   );
 }
