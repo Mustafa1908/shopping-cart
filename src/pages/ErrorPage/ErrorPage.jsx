fd9a9bc8af48373a256a9c26636b9047bc3af556
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-07-10 13:42:13 +0200

    Add errorPage text and links

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom";
+import LinkTo from "../../components/LinkTo/LinkTo";
+import errorPage from "./ErrorPage.module.css";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Oh no, this route doesn't exist!</h1>
-      <Link to="/">
-        You can go back to the home page by clicking here, though!
-      </Link>
+    <div className={errorPage.errorPageContainer}>
+      <h1>ERROR 404</h1>
+      <h2>This is weird</h2>
+      <p>
+        This page doesn't exist or is in construction. Sorry for the
+        inconvenience.
+      </p>
+      <span>Try the links below!</span>
+      <div className={errorPage.linkToContainer}>
+        <LinkTo LinkText={"MAINPAGE"} LinkPath={"/"} />
+        <LinkTo LinkText={"JEWELERY"} LinkPath={"/jewelery"} />
+        <LinkTo LinkText={"ELECTRONICS"} LinkPath={"/electronics"} />
+        <LinkTo LinkText={"MEN"} LinkPath={"/men"} />
+        <LinkTo LinkText={"WOMEN"} LinkPath={"/women"} />
+      </div>
     </div>
   );
 };
