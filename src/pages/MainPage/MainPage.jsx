8db594aade2d66fdb74c26d1bbc62b577e4c38ba
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-07-11 14:16:29 +0200

    Add div image container, add image text container, add paragraph and Link

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,17 @@
 import mainPage from "./MainPage.module.css";
+import { Link } from "react-router-dom";
 
 export default function MainPage() {
-  return <main className={mainPage.main}></main>;
+  return (
+    <main className={mainPage.main}>
+      <div className={mainPage.imageContainer}>
+        <div className={mainPage.imageTextContainer}>
+          <p className={mainPage.imageText}>DISCOVER OUR NEW TRENDY PRODUCTS</p>
+          <Link to={"./jewelery"} className={mainPage.imageLink}>
+            SHOP NOW
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
